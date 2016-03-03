96617f2dad414d56589a6a9d07a7ecbbeb8b48b5
mazafaka2026 <mazafaka2026@bk.ru>
2016-03-04 01:08:16 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,18 +1,20 @@
 import urlparse
 slovar=''
-def app(env, start_response):
-	lines = []
-	for key, value in environ.items():
-		suck=lines.append("%s: %r" % (key, value))
-		print (str(suck))
+def app(environ, start_response):
+	print environ
+	url=environ[QUERY_STRING]
+	#lines = []
+	#for key, value in environ.items():
+	#	suck=lines.append("%s: %r" % (key, value))
+	#	print (str(suck))
 	#env=url
-	#status='200 OK'
-	#headers = [
-	#	('Content-Type', 'text/plain')
-	#]
-	#body=slovar
-	#start_response(status, headers)
-	#return[body]
+	status='200 OK'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+	body=slovar
+	start_response(status, headers)
+	return[body]
 	
 
 url = "http://example.com/?a=1&a=2&c=123&d=6"
