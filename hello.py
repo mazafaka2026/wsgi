29a1a781b18221b5a6f212a2562f7fd8d2196b2e
mazafaka2026 <mazafaka2026@bk.ru>
2016-03-04 04:05:50 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,7 @@
 import urlparse
 slovar=''
-def app(environ, start_response):
-	print environ
-	url=environ[QUERY_STRING]
-	#lines = []
-	#for key, value in environ.items():
-	#	suck=lines.append("%s: %r" % (key, value))
-	#	print (str(suck))
-	#env=url
+def app (environ, start_response):
+	environ=arg
 	status='200 OK'
 	headers = [
 		('Content-Type', 'text/plain')
@@ -17,26 +11,14 @@ def app(environ, start_response):
 	return[body]
 	
 
-url = "http://example.com/?a=1&a=2&c=123&d=6"
+url = "http://example.com/?a=1&a=2&c=123&d=6&a=10"
 print url
 query = urlparse.urlparse(url).query
-#print query
+url2=query.split("&")
 
-params = urlparse.parse_qs(query)
+
+#params = urlparse.parse_qs(query)
 #print params
 
-for arg in params:
-	if (len(params[arg])) > 1: 
-		while True:
-			if (len(params[arg])) <= 0:
-				break
-			else:
-				slovar=(slovar)+((str(arg)+'='+str(params[arg][0]))) +'''
-'''
-				#print slovar
-				del(params[arg][0])
-	else:
-		slovar=(slovar)+(((str(arg)+'='+str(params[arg][0]))))+'''
-'''
-		#print slovar
-print slovar
+for arg in url2:
+	print arg
