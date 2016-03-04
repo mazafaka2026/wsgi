555ec655e00d3290d37e9b1ea040e85ca7fe877d
mazafaka2026 <mazafaka2026@bk.ru>
2016-03-04 16:36:01 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,3 @@
-sudo su
-apt-get install nginx
-pip install gunicorn
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/nginx/sites-enabled/test
