12884aacc38ee0adfa145d0de1e9ae187f9f3610
mazafaka2026 <mazafaka2026@bk.ru>
2016-03-04 17:20:30 +0600

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,30 +1,42 @@
-upstream example_mysite {
-
-  server 0.0.0.0:8080 fail_timeout=0;
+proxy_set_header Host $host;
+proxy_set_header X-Real-IP $remote_addr;
+location / {
+	proxy_pass http://beckend;
 }
-server {
-  listen 80;
-  client_max_body_size 4G;
-  server_name example.com www.example.com;
 
-  keepalive_timeout 5;
-
-  # path for static files
-  root /home/box/web;
-
-  location / {
-      # checks for static file, if not found proxy to app
-      try_files $uri @proxy_to_app;
-  }
-  location /hello/ {
-      alias /home/box/web;     
-  }
-
-  location @proxy_to_app {
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      proxy_set_header Host $http_host;
-      proxy_redirect off;
-      proxy_pass http://gunicorn_mysite;
-  }
-
+location /hello/ {
+	proxy_pass http://beckend;
 }
+upstream backend {
+	server 0.0.0.0:8080 weight=1;
+}
+#upstream example_mysite {
+#
+#  server 0.0.0.0:8080 fail_timeout=0;
+#}
+#server {
+#  listen 80;
+#  client_max_body_size 4G;
+#  server_name example.com www.example.com;
+#
+#  keepalive_timeout 5;
+#
+ # # path for static files
+#  root /home/box/web;
+#
+#  location / {
+#      # checks for static file, if not found proxy to app
+#      try_files $uri @proxy_to_app;
+#  }
+ # location /hello/ {
+#      alias /home/box/web;     
+ # }
+#
+#  location @proxy_to_app {
+#      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+ #     proxy_set_header Host $http_host;
+ #     proxy_redirect off;
+ #     proxy_pass http://gunicorn_mysite;
+ # }
+#
+#}
