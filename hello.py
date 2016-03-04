ef5db0a0ed5414a5d284722534ff00ab8c0d0bd1
mazafaka2026 <mazafaka2026@bk.ru>
2016-03-04 14:39:25 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,24 +1,31 @@
 import urlparse
 slovar=''
 def app (environ, start_response):
-	environ=arg
+	url=environ
+	print url
+	query = urlparse.urlparse(url).query
+	url_fix=query.split("&")
+	for slovar in url_fix:
+		print 'fix_url---'+ slovar
+	
 	status='200 OK'
 	headers = [
 		('Content-Type', 'text/plain')
 	]
 	body=slovar
+	print 'body.....'+body
 	start_response(status, headers)
 	return[body]
 	
 
-url = "http://example.com/?a=1&a=2&c=123&d=6&a=10"
-print url
-query = urlparse.urlparse(url).query
-url2=query.split("&")
+#url = "http://example.com/?a=1&a=2&c=123&d=6&a=10"
+#print url
+#query = urlparse.urlparse(url).query
+#url2=query.split("&")
 
 
 #params = urlparse.parse_qs(query)
 #print params
 
-for arg in url2:
-	print arg
+#for arg in url2:
+#	print arg
