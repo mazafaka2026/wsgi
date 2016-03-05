03582e2c2d423be69151e71c3e3894407e0d9c30
mazafaka2026 <mazafaka2026@bk.ru>
2016-03-05 11:16:19 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,19 +3,19 @@ slovar=''
 def app (environ, start_response):
 	url=environ
 	print url
-	query = urlparse.urlparse(url).query
-	url_fix=query.split("&")
-	for slovar in url_fix:
-		print 'fix_url---'+ slovar
+	#query = urlparse.urlparse(url).query
+	#url_fix=query.split("&")
+	#for slovar in url_fix:
+	#	print 'fix_url---'+ slovar
 	
-	status='200 OK'
-	headers = [
-		('Content-Type', 'text/plain')
-	]
-	body=slovar
-	print 'body.....'+body
-	start_response(status, headers)
-	return[body]
+	#status='200 OK'
+	#headers = [
+	#	('Content-Type', 'text/plain')
+	#]
+	#body=slovar
+	#print 'body.....'+body
+	#start_response(status, headers)
+	#return[body]
 	
 
 #url = "http://example.com/?a=1&a=2&c=123&d=6&a=10"
