744c01a8405bcbe8d9cf6bec00813a49554d91bd
mazafaka2026 <mazafaka2026@bk.ru>
2016-03-05 16:54:42 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,31 +1,31 @@
 import urlparse
 slovar=''
 def app (environ, start_response):
-	url=environ
+	url=''#environ['QUERY_STRING']
 	print url
-	#query = urlparse.urlparse(url).query
-	#url_fix=query.split("&")
-	#for slovar in url_fix:
-	#	print 'fix_url---'+ slovar
+	query = urlparse.urlparse(url).query
+	url_fix=query.split("&")
+	for slovar in url_fix:
+		print 'fix_url---'+ slovar
 	
-	#status='200 OK'
-	#headers = [
-	#	('Content-Type', 'text/plain')
-	#]
-	#body=slovar
-	#print 'body.....'+body
-	#start_response(status, headers)
+	status='200 OK'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+	body=slovar
+	print 'body.....'+body
+	start_response(status, headers)
+	if url == '':
+		print 'bed'
+	else:
+		print ''
+		print url
+		query = urlparse.urlparse(url).query
+		url_fix=query.split("&")
+		for slovar in url_fix:
+			print 'fix_url---'+ slovar
+			body=slovar
+			return[body]
 	#return[body]
 	
-
-#url = "http://example.com/?a=1&a=2&c=123&d=6&a=10"
-#print url
-#query = urlparse.urlparse(url).query
-#url2=query.split("&")
-
-
-#params = urlparse.parse_qs(query)
-#print params
-
-#for arg in url2:
-#	print arg
+print 'Hello'
